eec2a73f7689e7afb7efce0afe91c0934e6411b9
Analucia M <83628310+anilu-221@users.noreply.github.com>
2023-10-02 13:02:53 -0600

    Syntax

--- paradeportes-inc.php
+++ paradeportes-inc.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Plugin Name:       Paradeportes Inc
  * Description:       Funcionalidades para Paradeportes Inc.
@@ -9,18 +9,20 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       paradeportes
+ *
+ * @package paradeportes-inc
  */
 
 /** PLUGIN Constants*/
-define('PLUGIN_PATH', plugin_dir_path( __FILE__ ));
-define('PLUGIN_URL', plugin_dir_url( __FILE__ ));
+define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 /** Requires */
-require_once( PLUGIN_PATH . '/inc/custom-post-types.php' );
-require_once( PLUGIN_PATH . '/inc/files.php' );
-require_once( PLUGIN_PATH . '/inc/options-page.php' );
-require_once( PLUGIN_PATH . '/inc/theme-functions.php' );
-require_once( PLUGIN_PATH . '/inc/whatsapp-boton.php' );
+require_once PLUGIN_PATH . '/inc/custom-post-types.php';
+require_once PLUGIN_PATH . '/inc/files.php';
+require_once PLUGIN_PATH . '/inc/options-page.php';
+require_once PLUGIN_PATH . '/inc/theme-functions.php';
+require_once PLUGIN_PATH . '/inc/whatsapp-boton.php';
 
 /** Template Functions */
-require_once( PLUGIN_PATH . '/inc/template-functions/hero-banner.php' );
+require_once PLUGIN_PATH . '/inc/template-functions/hero-banner.php';
