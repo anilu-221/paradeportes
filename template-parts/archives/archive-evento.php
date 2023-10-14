7edc2bdc42be75f5cf30752a38feb68824fb510b
Analucia M <83628310+anilu-221@users.noreply.github.com>
2023-10-13 20:23:32 -0600

    Event archive buttons

--- template-parts/archives/archive-evento.php
+++ template-parts/archives/archive-evento.php
@@ -86,13 +86,15 @@ $eventos_anteriores_query = new WP_Query( $eventos_anteriores_args );
 											<div class="pd-card__body">
 												<!--Deporte-->
 													<?php
-													$deportes = get_the_terms( get_the_ID(), 'deporte' );
-													foreach ( $deportes as $deporte ) {
-														?>
-															<a class="text-decoration-none" href="<?php echo esc_url( get_term_link( $deporte->term_id ) ); ?>">
-																<p class="d-inline"><span class="badge dark-hover bg-primary me-2"><?php echo esc_html( $deporte->name ); ?></span></p>
-															</a>
-														<?php
+													$deportes = get_the_terms( get_the_ID(), 'paradeporte' );
+													if ( $deportes ) {
+														foreach ( $deportes as $deporte ) {
+															?>
+																<a class="text-decoration-none" href="<?php echo esc_url( get_term_link( $deporte->term_id ) ); ?>">
+																	<p class="d-inline"><span class="badge dark-hover bg-primary me-2"><?php echo esc_html( $deporte->name ); ?></span></p>
+																</a>
+															<?php
+														}
 													}
 													?>
 
@@ -106,6 +108,11 @@ $eventos_anteriores_query = new WP_Query( $eventos_anteriores_args );
 													<p>
 														Fecha: <?php echo wp_kses_post( $fecha ); ?>
 													</p>
+
+												<!--Boton-->
+													<a class="btn btn-sm btn-dark" href="<?php echo esc_url( get_the_permalink() ); ?>">
+														Ver Más
+													</a>
 											</div>
 									</div>
 								</div>
@@ -177,6 +184,10 @@ $eventos_anteriores_query = new WP_Query( $eventos_anteriores_args );
 													<p>
 														Fecha: <?php echo wp_kses_post( $fecha ); ?>
 													</p>
+												<!--Boton-->
+												<a class="btn btn-sm btn-dark" href="<?php echo esc_url( get_the_permalink() ); ?>">
+													Ver Más
+												</a> 
 											</div>
 									</div>
 								</div>
